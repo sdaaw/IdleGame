f36d59b7cdbfd93a6afda4c9268d7d2d1f5713b7
sdaaw <ma.oskari@gmail.com>
2018-11-08 19:06:44 +0200

    removed libraries
    removed unnecessary libraries

--- IdleGame/WorldPoint.cs
+++ IdleGame/WorldPoint.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IdleGame {
+﻿namespace IdleGame {
     class WorldPoint {
         public int x = 0;
         public int y = 0;
